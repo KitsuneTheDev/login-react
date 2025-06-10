1198324117760e0803612445f78502faa724d1ff
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2025-06-10 21:09:52 +0300

    update: Layout.jsx, Navbar.jsx
    - Navbar is created for mobile look
    - color added to theme.css

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,24 @@
-import LoginForm from "./LoginForm.jsx";
+import { useState } from 'react';
+import Navbar from './Navbar.jsx';
+import SignupForm from './SignupForm.jsx';
 
 function Layout() {
+
+    const isMobile = /mobile/i.test(navigator.userAgent);
+    console.log("isMobile -->", isMobile, "device type is -->", navigator.userAgent);
+    
     return(
-        <>
-            <header className="w-full h-[16%] bg-amber-500"></header>
-            <main className="w-full h-[76%] bg-amber-300">
-                <LoginForm />
+        <div className='min-h-[100vh] min-w-[100vw] h-[100vh] w-[100vw]'>
+            <header className="bg-background-secondary w-full h-[10%] flex border-b-2 border-accent">
+                {isMobile ? <Navbar.Mobile /> : <Navbar.Default />}
+            </header>
+            <main className="bg-background-primary w-full h-[86%]">
+                <SignupForm />
             </main>
-            <footer className="w-full h-[8%] bg-amber-600"></footer>
-        </>
+            <footer className="bg-background-secondary w-full h-[6%] border-t-2 border-accent">
+
+            </footer>
+        </div>
     );
 }
 
---
+++ src/components/SignupForm.jsx
@@ -0,0 +1,9 @@
+function SignupForm() {
+    return(
+        <div className="signup-form-container">
+
+        </div>
+    );
+}
+
+export default SignupForm;
